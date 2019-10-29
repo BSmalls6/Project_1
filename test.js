4e1e9cd0641dd0fe2e888b2045f655b826742ea5
Brennan Smallwood <39069788+BSmalls6@users.noreply.github.com>
2019-10-28 17:04:11 -0700

    shuffle and draw cards from API
    takes the api call, shuffles and draws cards based on the amount of players.  need to add distribution to piles

---
+++ test.js
@@ -0,0 +1,97 @@
+// Shuffle new deck
+var deckId = "";
+var playerPile = "";
+var dealerPile = ""
+var players = ["player", "dealer"];
+$("#go").on("click", function (event) {
+    event.preventDefault();
+
+    // Here we construct our URL
+    var shuffleDeck = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+    // Ajax call for current weather
+    $.ajax({
+        url: shuffleDeck,
+        method: "GET"
+    }).then(function (response) {
+        deckId = response.deck_id;
+
+        initialHand();
+
+    });
+    function initialHand() {
+
+        var drawCardsurl = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=" + ((players.length) * 2);
+
+        $.ajax({
+            url: drawCardsurl,
+            method: "GET"
+        }).then(function (draw) {
+            var firstDeal = (draw.cards);
+            // playerPile = 
+            console.log(firstDeal.length);
+
+
+            // get cards loop:
+            for (i = 0; i < firstDeal.length; i++) {
+              var curCard = firstDeal[i];
+                var numPlayers = players.length;
+                var roundsDealt = 0;
+            console.log(curCard);
+            console.log(numPlayers);
+            };
+function dealCards {
+                while (roundsDealt < 1) {
+                    for (var j = 0; j < numPlayers; i++) {
+                        var curPlayer = numPlayers[j];
+                        // giveCardToPlayer(curPlayer, curCard);
+                        console.log(curPlayer);
+                        console.log(curCard);
+                        addCards();
+                        roundsDealt++;
+
+                    }
+                }
+            };
+            // function giveCardToPlayer(curPlayer, curCard) {
+            //     console.log(curPlayer, curCard);
+                //     // ajax to add card to pile for player
+                // https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/add/?cards=AS,2S
+            }
+            
+            
+        });
+    };
+
+
+});
+
+// function dealHand(players) {
+//     // ajax to pull next player.length * 2 cards from the deck\
+//     // loop to give cards to players
+//     // 1 - to player
+//     giveCardToPlayer(players[0]);
+//     // 2 - to dealer
+//     giveCardToPlayer(players[1]);
+// }
+
+
+// function startGame() {
+//     dealHand(players);
+// }
+
+
+
+
+
+
+// add cards to piles
+// https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/add/?cards=AS,2S
+
+
+// shuffle deck
+// https://deckofcardsapi.com/api/deck/<<deck_id>>/shuffle/
+
+
+// list cards in piles
+// https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/list
