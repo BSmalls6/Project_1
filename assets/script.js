be73e191cd3c139f01beccabc577ff86e8ea8a9b
Serina Tsang <serinat@uw.edu>
2019-10-31 02:58:23 -0700

    increase and decrease buttons work

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
-var increaseBet = 10;
 $(document).ready(function () {
+    var bet = 10;
     $("#chipI").on("click", function (event) {
-        increaseBet+=5;
-        $("#minBet").html(increaseBet);
-var decreaseBet = 10;             
-        $("#chipD").on("click", function (event) {
-        decreaseBet-=5;
-        $("#minBet").html(decreaseBet);
-        });
-    });
+        if (bet < 100) {
+            bet += 5;
+            $("#minBet").html("$" + bet);
+        }
+    })
+    $("#chipD").on("click", function (event) {
+        if (bet >= 15) {
+            bet -= 5;
+            $("#minBet").html("$" + bet);
+        }
+    })
 });
+
